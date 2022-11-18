ff9acae7d68b013fac2ae923076737012767fbcc
Borza Zsolt <borza.zsolt89@gmail.com>
2022-11-18 12:02:29 +0100

    Initial Commit

---
+++ TotalNBA/TotalNBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalNBAApp.swift
+//  TotalNBA
+//
+//  Created by Borza Zsolt on 2022. 11. 18..
+//
+
+import SwiftUI
+
+@main
+struct TotalNBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
